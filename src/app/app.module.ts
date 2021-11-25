2e207c35737e40029b773109da42384e051f4395
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2021-11-25 21:09:14 +0100

    New files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
 import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
