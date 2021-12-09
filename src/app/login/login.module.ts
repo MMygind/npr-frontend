271ef570ab51c71c0b82eb35bf63cf7fb5d3d2b5
Troels <31724037+tr0els@users.noreply.github.com>
2021-12-09 11:36:42 +0100

    Adding material to module

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialExampleModule } from 'src/material.module';
 
 
 @NgModule({
@@ -13,6 +13,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     LoginRoutingModule,
+    MaterialExampleModule,
+    FormsModule,
     ReactiveFormsModule
   ]
 })
